e2eb0c0a99fad632bae1522d470cb172b715c6e0
AmbyMbayi <ambymbayi54@gmail.com>
2020-12-14 17:20:27 +0300

    scrollup add

--- assets/js/main.js
+++ assets/js/main.js
@@ -56,3 +56,14 @@ function scrollHeader(){
 }
 
 window.addEventListener('scroll', scrollHeader)
+
+
+/* show scroll up*/
+function scrollTop(){
+    const scrollTop = document.getElementById('scroll-top')
+
+    if(this.scrollY >= 560) scrollTop.classList.add('show-scroll');
+    else scrollTop.classList.remove('show-scroll')
+}
+
+window.addEventListener('scroll', scrollTop) 
